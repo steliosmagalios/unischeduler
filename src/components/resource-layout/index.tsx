9e1e02bc660f67bf76c5dfce8c21919ebe2e6d1b
Stelios Magalios <47584641+steliosmagalios@users.noreply.github.com>
2023-06-29 13:08:46 +0300

    placeholder dialog for creating items

--- src/components/resource-layout/index.tsx
+++ src/components/resource-layout/index.tsx
@@ -1,5 +1,16 @@
+import { createTsForm, useTsController } from "@ts-react/form";
+import { z } from "zod";
 import DashboardLayout from "~/components/dashboard-layout";
-import { DataTable, DataTableProps } from "~/components/data-table";
+import { DataTable, type DataTableProps } from "~/components/data-table";
+import { Button } from "~/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
 import { capitalize } from "~/utils/lib";
 
 type ResourceLayoutProps<TData extends { id: string }, TType> = {
@@ -25,11 +36,58 @@ export default function ResourceLayout<TData extends { id: string }, TType>(
         </div>
 
         <div className="flex flex-row-reverse">
-          <button className="rounded-md bg-blue-500 px-4 py-2 font-semibold">
-            Create
-          </button>
+          <Dialog modal>
+            <DialogTrigger asChild>
+              <Button className="rounded-md bg-blue-500 px-4 py-2 font-semibold">
+                Create
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="w-[90%]">
+              <DialogHeader>
+                <DialogTitle>Create {props.label}</DialogTitle>
+              </DialogHeader>
+
+              <MyForm
+                schema={groupSchema}
+                onSubmit={(data: z.infer<typeof groupSchema>) =>
+                  console.log(data)
+                }
+                renderAfter={() => (
+                  <DialogFooter>
+                    <Button variant="ghost">Cancel</Button>
+                    <Button type="submit">Create</Button>
+                  </DialogFooter>
+                )}
+              />
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
     </DashboardLayout>
   );
 }
+
+const groupSchema = z.object({
+  name: z.string().nonempty(),
+});
+
+function TextIn() {
+  const { field, error } = useTsController<string>();
+  return (
+    <>
+      <input
+        className="text-black"
+        type="text"
+        value={field.value ? field.value : ""}
+        onChange={(e) => {
+          field.onChange(e.target.value);
+        }}
+      />
+      {error?.errorMessage && <span>{error?.errorMessage}</span>}{" "}
+    </>
+  );
+}
+
+const mapping = [[z.string(), TextIn] as const] as const;
+
+const MyForm = createTsForm(mapping);
