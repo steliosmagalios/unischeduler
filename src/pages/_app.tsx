2502668f60b67ec52422365df721abe81e84913d
Stelios Magalios <47584641+steliosmagalios@users.noreply.github.com>
2023-07-05 13:35:16 +0300

    added a toaster

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/app";
+import { Toaster } from "~/components/ui/toaster";
 import "~/styles/globals.css";
 import { api } from "~/utils/api";
 
@@ -7,6 +8,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
       <Component {...pageProps} />
+      <Toaster />
     </ClerkProvider>
   );
 };
