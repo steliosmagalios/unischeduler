ce93efad1e38ca14757ab25384c3ac14fd9c3e3a
Stelios Magalios <47584641+steliosmagalios@users.noreply.github.com>
2023-07-18 10:25:50 +0300

    saving course preferences displays toast

--- src/components/manage-courses-dialog/index.tsx
+++ src/components/manage-courses-dialog/index.tsx
@@ -11,9 +11,13 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "~/components/ui/dialog";
+import { useToast } from "~/components/ui/use-toast";
 import { api } from "~/utils/api";
 
 export default function ManageCoursesDialog() {
+  const { toast } = useToast();
+  const [dialogOpen, setDialogOpen] = useState(false);
+
   const [rowSelection, setRowSelection] = useState<RowSelectionState>({});
   const { data: courses, isLoading: coursesLoading } =
     api.course.getAll.useQuery();
@@ -33,11 +37,20 @@ export default function ManageCoursesDialog() {
   }
 
   function onSave() {
+    // FUTURE: this could be a context just to provide info to more components
     window.localStorage.setItem("rowSelection", JSON.stringify(rowSelection));
+
+    toast({
+      title: "Course preferences saved",
+      description: "The course preferences have been saved successfully.",
+      duration: 1500,
+    });
+
+    setDialogOpen(false);
   }
 
   return (
-    <Dialog modal>
+    <Dialog modal open={dialogOpen} onOpenChange={setDialogOpen}>
       <DialogTrigger asChild>
         <Button variant="ghost" size="sm">
           Manage
