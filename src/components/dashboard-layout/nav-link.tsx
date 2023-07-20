ac8829ea8493a5fb5f34bf327dee97e696a9b966
Stelios Magalios <47584641+steliosmagalios@users.noreply.github.com>
2023-07-20 16:53:37 +0300

    updated nav-link background

--- src/components/dashboard-layout/nav-link.tsx
+++ src/components/dashboard-layout/nav-link.tsx
@@ -12,7 +12,7 @@ export default function NavLink(props: NavLinkProps) {
     <Link
       data-active={!!props.active || undefined} // this is awful, plz fix
       href={props.href}
-      className="p-2 data-[active]:border-l-4 data-[active]:border-white data-[active]:bg-neutral-500/50 data-[active]:font-bold"
+      className="rounded-md from-slate-700 p-2 data-[active]:bg-gradient-to-r data-[active]:font-bold"
     >
       {props.label}
     </Link>
