7cc0b0fa26af7c015ac54ec8bde10de8de9e3f88
Stelios Magalios <47584641+steliosmagalios@users.noreply.github.com>
2023-08-09 08:05:41 +0300

    clerk dark theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import { type AppType } from "next/app";
 import { ThemeProvider } from "~/components/theme-provider";
 import { Toaster } from "~/components/ui/toaster";
@@ -8,7 +9,7 @@ import { api } from "~/utils/api";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps}>
+    <ClerkProvider appearance={{ baseTheme: dark }} {...pageProps}>
       <ThemeProvider defaultTheme="dark" attribute="class">
         <TooltipProvider>
           <Component {...pageProps} />
